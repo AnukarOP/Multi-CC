b3eaefe83538982ca7637211ffc05c5bfa86d1c0
AnukarOP <124155412+AnukarOP@users.noreply.github.com>
2023-10-14 10:26:46 +0530

    Update cc.py

--- cc.py
+++ cc.py
@@ -30,9 +30,9 @@ def logo():
     figlet = Figlet(font="standard").renderText("Fox CC")
     return (gn + figlet)
 print (logo())
-print (bl + "[-] Powered by Black Fox Security Team ")
-print (gn + "[+] Made By Maximum Radikali")
-print (cy + "[=] Fox CC Tools Version : 1.1")
+print (bl + "[-] Powered by Team HH [Hello Hackers] ")
+print (gn + "[+] Script By AnukarOP")
+print (cy + "[=] Multi-CC Tool Version : 1.0")
 
 opr = input (mag + "\n[x] 1) Generate single valid cc\n[x] 2) Generate multi valid cc (generate cc list)\n[x] 3) CC validator\n[x] 4) Generate Multi Bin Number \n\n[^] Please Enter an option :  ")
 
@@ -44,7 +44,7 @@ def genscard():
     rs = requests.post(sitex , headers=headers , cookies=cookies,data=payload)
     data = json.loads(rs.text)
     card = data['creditCard'][1]
-    return (gn + "[-] Brand : %s\n[-] Card Number : %s\n[-] Bank : %s\n[-] Name : %s\n[-] Address : %s\n[-] Country : %s\n[-] Money Range : %s\n[-] CVV : %s\n[-] Expiry : %s\n[-] Pin : %s\n============================\n[*] Telegram Channel : @BlackFoxSecurityTeam" % (card['IssuingNetwork'] , card['CardNumber'] , card['Bank'] , card['Name'] , card['Address'] , card['Country'] , card['MoneyRange'] , card['CVV'] , card['Expiry'] , card['Pin']) + cv)
+    return (gn + "[-] Brand : %s\n[-] Card Number : %s\n[-] Bank : %s\n[-] Name : %s\n[-] Address : %s\n[-] Country : %s\n[-] Money Range : %s\n[-] CVV : %s\n[-] Expiry : %s\n[-] Pin : %s\n============================\n[*] Script by @AnukarOP" % (card['IssuingNetwork'] , card['CardNumber'] , card['Bank'] , card['Name'] , card['Address'] , card['Country'] , card['MoneyRange'] , card['CVV'] , card['Expiry'] , card['Pin']) + cv)
 
 def genmcard():
     cookies = {"csrftoken":"8b56rI96TwUH0X7dOT86JmPMBbUVYEpX3EI7ZKp3ZXHWnrRySD9ORyNaAaRXnW7i","_ga":"GA1.2.1579916434.1654760883","_gid":"GA1.2.1410860416.1654760883","_gads":"ID=d4f0fe2265535514-2243e178fad30069:T=1654760893:RT=1654760893:S=ALNI_MaIzJo5Kmg3rKoLXSuvDGnQkyW3uw","_gpi":"UID=0000087f297f7f43:T=1654760893:RT=1654760893:S=ALNI_MbnajBnRWmSHW7vrpR-U1w2uMwyVw",'FCNEC':'[["AKsRol_6etCde6kaPNd_o13SF2anvKLy0qaXvN6Kz0O_d9YbYS_KOfZ-j0xDjsEXL_4Otx5R38juHOOwfg0JShy5DHGmgAw2R6ZN4KZyI3qGimMjR0mQ0SEgj2ncvV4jQ32pssYst9ml2ptS_Ip2XyPbrLivgKXjIQ=="],null,[]]'}
@@ -57,8 +57,8 @@ def genmcard():
     for i in range(1,10):
         card = data['creditCard'][i]
         f = open("generated_card.txt","a")
-        f.write("[-] Brand : %s\n[-] Card Number : %s\n[-] Bank : %s\n[-] Name : %s\n[-] Address : %s\n[-] Country : %s\n[-] Money Range : %s\n[-] CVV : %s\n[-] Expiry : %s\n[-] Pin : %s\n===================================\n" % (card['IssuingNetwork'] , card['CardNumber'] , card['Bank'] , card['Name'] , card['Address'] , card['Country'] , card['MoneyRange'] , card['CVV'] , card['Expiry'] , card['Pin']))
-    return (gn + "[$] The operation has been success\n[+] Saved File as generated_card.txt" + cv)
+        f.write("[-] Brand : %s\n[-] Card Number : %s\n[-] Bank : %s\n[-] Name : %s\n[-] Address : %s\n[-] Country : %s\n[-] Money Range : %s\n[-] CVV : %s\n[-] Expiry : %s\n[-] Pin : %s\n===================================\n[*] Script by @AnukarOP" % (card['IssuingNetwork'] , card['CardNumber'] , card['Bank'] , card['Name'] , card['Address'] , card['Country'] , card['MoneyRange'] , card['CVV'] , card['Expiry'] , card['Pin']))
+    return (gn + "[$] The operation has been success\n[+] Saved File as generated_card.txt || Script by @AnukarOP" + cv)
 
 def ccvalidator(number , type):
     site = "https://www.tools4noobs.com/"
@@ -91,4 +91,4 @@ elif opr == "4":
     print (rd)
     bin.bin_generator(number , round)
     print ("Saved File as bin_generated.txt !")
-    print (mag + "[$] Telegram Channel : @BlackFoxSecurityTeam" + cv)
+    print (mag + "[$] Script by AnukarOP" + cv)
